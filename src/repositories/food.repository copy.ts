ca68619eb94bd738df3c060d377a6bf1012051db
sunidsa <meen_sunidsa@kkumail.com>
2021-07-30 12:57:05 +0600

    update entity

--- src/repositories/food.repository copy.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-import { Food } from '../entity/food.entity'
-
-@EntityRepository(Food)
-export class FoodRepository extends Repository<Food> {}
--- src/repositories/refresh-token.repository copy.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-import { RefreshToken } from '../entity/refresh-token.entity'
-
-@EntityRepository(RefreshToken)
-export class RefreshTokenRepository extends Repository<RefreshToken> {}
